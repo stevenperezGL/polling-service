fa4c5123daa9b58b2c4a04565f70f4f049a12b7a
Maria Jose Cubero <maria.cubero@gorillalogic.com>
2019-06-20 11:26:03 -0600

    Add get

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const app = require('express')();
+var http = require('http').createServer(app);
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-    console.log(`Server running in port ${port}`);
+// app.listen(port, () => {
+//     console.log(`Server running in port ${port}`);
+// });
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello world</h1>');
 });
 
-
-
+http.listen(port, function(){
+    console.log(`Server running in port ${port}`);
+});
