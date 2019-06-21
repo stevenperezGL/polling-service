430ceefa79954eecd21bcd18ac233b1796e872fd
Maria Jose Cubero <maria.cubero@gorillalogic.com>
2019-06-21 16:52:02 -0600

    Add fibonacci options.

--- events/options-events.js
+++ events/options-events.js
@@ -3,12 +3,14 @@ const { getOptions } = require('../app');
 const optionsEvents = function(socket){
 
     function getOptionsFromMongo(){
+        const options = ['1', '2', '3', '5', '8', '13', '21'];
         socket.on('get-options', function (secretKey) {
-            getOptions().then(response =>
-                socket.emit(`get-options-${secretKey}`, JSON.stringify(response)))
-                .catch((reason =>
-                    console.log(`Rejected promise. Reason => ${reason}`)
-                ));
+            socket.emit(`get-options-${secretKey}`, JSON.stringify(options));
+            // getOptions().then(response =>
+            //     socket.emit(`get-options-${secretKey}`, JSON.stringify(response)))
+            //     .catch((reason =>
+            //         console.log(`Rejected promise. Reason => ${reason}`)
+            //     ));
         });
     }
 
