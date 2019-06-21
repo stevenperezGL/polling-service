e75e4d10dfd63c0937f5aa368072784033682cca
Maria Jose Cubero <maria.cubero@gorillalogic.com>
2019-06-21 11:52:16 -0600

    add headers.

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const { optionsEvents } = require('./events/options-events');
 
 app.use(cors());
 
+app.all('*', function(req, res, next) {
+    var origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 server.use(cors());
 
 app.get('/', function(req, res){
